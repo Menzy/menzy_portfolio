e814abc921de476df8e83e8af3da26db80a5e7a1
Menzy <joshuamensah46@gmail.com>
2025-01-30 20:46:40 +0000

    Remove unused import of useState and useEffect in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "next-themes";
 import { Toaster } from "@/components/ui/toaster";
