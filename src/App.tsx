a2d3c504ca3a813fc93351768e02c5921744aace
WanMenzy <joshuamensah46@gmail.com>
2025-08-20 23:26:41 +0000

    refactor(App): simplify loading state management and streamline component rendering

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "next-themes";
-import { useState } from "react";
-import { Toaster } from "@/components/ui/toaster";
+import { Toaster } from "@/components/ui/sonner";
+import { Analytics } from "@vercel/analytics/react";
 import { SocialSidebar } from "@/components/SocialSidebar";
-import { Loader } from "@/components/Loader";
 import { HomePage } from "@/pages/HomePage";
 import { ShopPage } from "@/pages/ShopPage";
 import { ProductPage } from "@/pages/ProductPage";
@@ -13,37 +12,26 @@ import { PrivacyPolicyPage } from "@/pages/PrivacyPolicyPage";
 import { TermsOfServicePage } from "@/pages/TermsOfServicePage";
 import { EchoPage } from "@/pages/EchoPage";
 import { EchoAuthCallbackPage } from "@/pages/EchoAuthCallbackPage";
-import { Analytics } from "@vercel/analytics/react";
 
 function App() {
-  const [isLoading, setIsLoading] = useState(true);
-
-  const handleLoadingComplete = () => {
-    setIsLoading(false);
-  };
-
   return (
     <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-      {isLoading ? (
-        <Loader onLoadingComplete={handleLoadingComplete} />
-      ) : (
-        <Router>
-          <SocialSidebar />
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/shop" element={<ShopPage />} />
-            <Route path="/shop/:slug" element={<ProductPage />} />
-            <Route path="/timelapse" element={<TimelapseAppPage />} />
-            <Route path="/timelapse/support" element={<TimelapseSupportPage />} />
-            <Route path="/echo" element={<EchoPage />} />
-            <Route path="/echo/auth/callback" element={<EchoAuthCallbackPage />} />
-            <Route path="/privacy-policy" element={<PrivacyPolicyPage />} />
-            <Route path="/terms" element={<TermsOfServicePage />} />
-          </Routes>
-          <Toaster />
-          <Analytics />
-        </Router>
-      )}
+      <Router>
+        <SocialSidebar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/shop" element={<ShopPage />} />
+          <Route path="/shop/:slug" element={<ProductPage />} />
+          <Route path="/timelapse" element={<TimelapseAppPage />} />
+          <Route path="/timelapse/support" element={<TimelapseSupportPage />} />
+          <Route path="/echo" element={<EchoPage />} />
+          <Route path="/echo/auth/callback" element={<EchoAuthCallbackPage />} />
+          <Route path="/privacy-policy" element={<PrivacyPolicyPage />} />
+          <Route path="/terms" element={<TermsOfServicePage />} />
+        </Routes>
+        <Toaster />
+        <Analytics />
+      </Router>
     </ThemeProvider>
   );
 }
